fd72580baa0aa7c2428fb975f8110c5da126cddd
chubuntuarc <arciniega.chubuntu@gmail.com>
2015-08-11 12:49:00 -0600

    Pruebas diseño

---
+++ js/plantilla.js
@@ -0,0 +1,22 @@
+// Comment out the following function call to manually use the menu toggle grippy
+var $interval = setInterval(function() {
+  menuToggleClickHandler();
+}, 2000);
+
+var $menuToggle = $('.menu-toggle'),
+  $body = $('body');
+
+function menuToggleClickHandler() {
+  $body.toggleClass('panel-open');
+  $menuToggle.toggleClass('open');
+}
+
+$menuToggle.click(function() {
+  menuToggleClickHandler();
+  clearInterval($interval);
+});
+
+$('.hidden-panel-close').click(function() {
+  $body.removeClass('panel-open');
+  $menuToggle.removeClass('open');
+});
